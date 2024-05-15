14e52dae93c2472d2fd773cec3d4177e961a1c0d
mhetar_adinath <mhetaradinath2412@gmail.com>
2024-05-15 17:17:55 +0530

    Added Question_56

---
+++ src/test/java/Questions/Question_56.java
@@ -0,0 +1,11 @@
+//Java program to print the Version of Java installed in your machine
+package Questions;
+
+public class Question_56 {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("java.version"));
+
+	}
+
+}
