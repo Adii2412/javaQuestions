57cc75ac80b3f00ab338a0cdf3b3dc3bbfc9d2f9
mhetar_adinath <mhetaradinath2412@gmail.com>
2024-05-16 16:01:18 +0530

    added question 58

---
+++ src/test/java/Questions/Question_58.java
@@ -0,0 +1,14 @@
+//Java program to find the ascii value of the given character
+package Questions;
+
+public class Question_58 
+{
+
+	public static void main(String[] args) 
+	{
+		char a ='a';
+		System.out.println((int)a);
+		
+	}
+
+}
