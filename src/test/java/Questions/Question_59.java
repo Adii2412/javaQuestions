39ba7d854ac7c8d0f4c4ef8d83af0bc09c23a81f
mhetar_adinath <mhetaradinath2412@gmail.com>
2024-05-17 19:11:33 +0530

    added Question 59 & 60

---
+++ src/test/java/Questions/Question_59.java
@@ -0,0 +1,11 @@
+//Java program to print the text in the specified format
+package Questions;
+
+public class Question_59 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("This is statement one.\nThis is statement two.\n\tThis is statement three\n\t\tThis is statement four.\nThis is statement five.");
+	}
+
+}
---
+++ src/test/java/Questions/Question_60.java
@@ -0,0 +1,15 @@
+//Java program to print the current system date and time
+package Questions;
+
+import java.util.Date;
+
+public class Question_60 
+{
+	public static void main(String[] args) 
+	{
+		Date date= new Date();
+		
+		System.out.println(date);
+	
+	}
+}
